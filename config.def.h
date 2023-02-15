dca3f91e927836613d366c07edbef7cb28ce0544
柚子 <40852301+uiYzzi@users.noreply.github.com>
2023-02-16 01:23:55 +0800

    :bulb: Add comments to config.def.h

--- config.def.h
+++ config.def.h
@@ -1,11 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 /* 有关版权和许可证的详细信息，请参阅许可证文件。 */
 
+/* 这是dwm的配置文件，关于这个文件更详细的说明可查看官网文档 https://dwm.suckless.org/customisation/ */
+
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+/* 外观 */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */	/* 窗口边框像素 */
+static const unsigned int snap      = 32;       /* snap pixel */				/* */
+static const int showbar            = 1;        /* 0 means no bar */			/* 0表示没有bar */
+static const int topbar             = 1;        /* 0 means bottom bar */		/* 0表示底部bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -20,6 +23,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
+/* 置标记 */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -28,24 +32,37 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
+	/* 类名	      实例         标题        标签掩码       是否浮动      显示器*/
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
+/* 布局 */
+
+// 主窗口区域的大小，也就是说，可以通过设置一个0.05到0.95之间的数值来控制主窗口区域的大小
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+
+// 主窗口区域中的客户端数量
 static const int nmaster     = 1;    /* number of clients in master area */
+
+// 是否遵循窗口自定义大小,1表示在平铺大小中的尊重窗口自定义的大小
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+
+// 是否强制将焦点聚焦到全屏窗口上，1表示强制将焦点集中在全屏幕窗口
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+// 它指定了dwm中可以使用的布局，比如tile、monocle等，每个布局都有一个对应的符号，用来表示该布局的功能
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	/* 符号        布局函数*/
+	{ "[]=",      tile },    /* first entry is default */                      /* 第一个条目是默认的 */
+	{ "><>",      NULL },    /* no layout function means floating behavior */  /* 没有布局函数意味着浮动行为 */
 	{ "[M]",      monocle },
 };
 
 /* key definitions */
+/* 键盘快捷键 */
 #define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
@@ -54,9 +71,11 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
+/* dwm中用来在早期版本中运行shell命令的辅助函数，它可以帮助用户在dwm-5.0之前的版本中运行shell命令 */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
+/* 命令 */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
@@ -98,6 +117,8 @@ static const Key keys[] = {
 };
 
 /* button definitions */
+/* 鼠标按钮 */
+
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
